8a235ea94a1d6efa38fa48b3272eb300fb62eb82
krzycho2 <36821768+krzycho2@users.noreply.github.com>
2018-03-23 10:18:30 +0100

    Poprawnie działająca średnia i odchylenie standardowe

--- Algorytm_region_growing2.py
+++ Algorytm_region_growing2.py
@@ -18,48 +18,34 @@ def srednia(_tab, _prog): # Niezbyt optymalna metoda
                     suma+=_tab[m,n]
                     iter+=1
                 
-#        !sr=suma / iter
-#       ! print(suma,iter)
+        sr=suma / iter
         return(sr)
     else:
         print("Niewłaściwa tablica")
     
 def odch_std(_tab, _prog):
-    sr = srednia(_tab, _prog)
-    suma=0
-    iter=0
-    [rows, columns] = _tab.shape
-    if rows>0 and columns>0:
-        for m in range(0, rows):
-            for n in  range(0, columns):
-                if _tab[m,n] > _prog:
-                    suma+=_tab[m,n]
-                    iter+=1
+    war = srednia( (_tab - srednia(_tab, _prog) )**2, _prog)
+    od_std = np.sqrt(war)
+#    
+    return(od_std)
                 
-#        !sr=suma / iter
-#       ! print(suma,iter)
-        return(sr)
-    else:
-        print("Niewłaściwa tablica")
-                
-def region_growing_by_gray_values(_tab, c, _prog, startingPoint = [0, 0] ):
+def region_growing_by_gray_values(_tab, _prog, _c=1, _startPoint = [0, 0] ):
 
-
+# startPoint przyda się później
 # ̣ Tablica może być 2-D lub 3-D w ogólnosci
 # c - parametr - mnoznik odchylenia standardowego we wzorze na predykat:
 # M(x) = | f(x) - avg | <= c * sigma
 # 
 
-
     avg=srednia(_tab, _prog)  
-    sigma_c = c * odch_std(_tab, _prog)
+    sigma_c = _c * odch_std(_tab, _prog)
     [rows, columns] = _tab.shape
     nowa_tab = np.zeros([rows, columns])
     for m in range(0, rows):
         for n in range(0, columns):
             if _tab[m,n] > _prog:
-                if tab[m,n] - avg <= sigma_c:
-                    nowa_tab[m,n] = tab[m,n]
+                if _tab[m,n] - avg <= sigma_c:
+                    nowa_tab[m,n] = _tab[m,n]
                     
     return(nowa_tab)
                     
@@ -77,15 +63,14 @@ tabliczka[3,0] = 200
 tabliczka[3,1] = 210
 tabliczka[4,0] = 190
 tabliczka[4,1] = 200
+
+prog = 100
 #print(tabliczka)
 
-(n, bins) = np.histogram(tabliczka)
-prog = np.argmin(n) # Z tym własnie nie mogłem sobie poradzić:
+# (n, bins) = np.histogram(tabliczka)
+# prog = np.argmin(n) 
+# Z tym własnie nie mogłem sobie poradzić:
 # Jak odczytać z histogramu odciętą - czyli szukany próg.
 
-
-tab = np.array([[1,2],
-                [2,3]])
-prog=0
-sr = srednia(tabliczka,100)
-
+tab = region_growing_by_gray_values(tabliczka, prog)
+print(tab)
