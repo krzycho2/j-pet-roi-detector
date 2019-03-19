068603cef4eeac16cd62fdd3ac4756eac8deb9b2
Krzysztof Krupiński <kkrupins@mion.elka.pw.edu.pl>
2019-03-19 23:33:10 +0100

    dodany zapis zmiennych do słoika

--- Fantom/read_data.py
+++ Fantom/read_data.py
@@ -1,22 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
+import pickle
 data = []
+fantomPath = '/home/krzysztof/Dokumenty/Praca_inż/j-pet-roi-detector/Fantom'
 
 # Odczytanie danych z pliku tekstowego
-fantomPath = 'Fantom/fantom2.txt'
-with open(fantomPath) as f:
+rawPath = fantomPath + '/fantom2.txt'
+with open(rawPath) as f:
     lines = f.readlines()
 
 for s in lines:
     a = s.split()
     data.append(a)
-data = np.array(data, dtype="float32")  # Konwersja do numpy.array
+data3D = np.array(data, dtype="float32")  # Konwersja do numpy.array
+
+# Zapis zmiennej do pliku
+dataFilePath = fantomPath + '/data.pckl'
+with open(dataFilePath, 'wb') as f:
+	pickle.dump(data3D, f)
 
 dataz0 = []
 
 # z_unique - Niepowtarzające się z -> Slice'y na różnych wysokościach
-z_unique = np.array(list(set(data[:,2])))
+z_unique = np.array(list(set(data3D[:,2])))
 
 # Sprawdzenie liczby slice'ów:
 # liczba_sliecow = len(z_unique)
@@ -24,9 +31,9 @@ z_unique = np.array(list(set(data[:,2])))
 # Wybór slice'u
 z0 = z_unique[18]
 
-for i in range(len(data)):
-    if data[i,2] == z0:
-        c = np.concatenate((data[i,:2], data[i,3:])) 
+for i in range(len(data3D)):
+    if data3D[i,2] == z0:
+        c = np.concatenate((data3D[i,:2], data3D[i,3:])) 
         dataz0.append(c)
 
 dataz0 = np.array(dataz0)
--- Progowanie/progowanie.py
+++ Progowanie/progowanie.py
@@ -1,1 +1,8 @@
 import numpy as np
+import pickle
+
+dataFilePath = '/home/krzysztof/Dokumenty/Praca_inż/j-pet-roi-detector/Fantom/data.pckl'
+with open(dataFilePath) as f:
+    data3D = pickle.load(f)
+
+# Histogram próbek
