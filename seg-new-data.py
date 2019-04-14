bf6ebf463e203880e7c2330c431f3006598c22ca
Krzysztof Krupiński <kkrupins@mion.elka.pw.edu.pl>
2019-04-14 14:13:18 +0200

    Poprawki czytelności wczytywania nowych rekonstrukcji

--- seg-new-data.py
+++ seg-new-data.py
@@ -3,17 +3,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pickle
-from lib import VolumeData,getListOfFiles
+from lib import VolumeData, getListOfFiles
 
-# Wczytywanie obrazów
+# Wczytywanie obrazów - podać ścieżkę do folderu z rozpakowanymi plikami txt. Mogą to być też pliki pickle
 fantomPath = '/home/krzysztof/Dokumenty/Praca_inż/j-pet-roi-detector/Fantom'
 files = getListOfFiles(fantomPath + '/Nowe-reko')
 volList = []
 leng = len(files)
+
+# Wczytanie danych do listy obiektów VolumeData
 for i in range(leng):
     print(f'Rekonstrukcja nr {i} z {leng}')
     vol = VolumeData(files[i])
     volList.append(vol)
 
+# Zaprezentowanie wszystkich przekrojów każdej rekonstrukcji - odkomentować
+"""
 for volume in volList:
     volume.showAllSlicesInOne()
+"""
+# Ewentualnie pokazanie poszczególnych slice'ów - odkomentować 
+"""
+vol0 = volList[0]
+sliceNum = 10
+slice1 = vol0.getSlice(sliceNum=sliceNum)
+plt.imshow(slice1)
+plt.title(vol0._fileName)
+plt.show()
+"""
