616edf74eb3e834e2f0c011eaf8fe69da55c4d57
Krzysztof Krupiński <kkrupins@mion.elka.pw.edu.pl>
2019-05-14 17:24:45 +0200

    Przeniesienie implementacji algorytmów do VolumeData

--- volumeData.py
+++ volumeData.py
@@ -6,7 +6,9 @@ import random
 import itertools
 from timeit import default_timer as timer
 from skimage.filters import threshold_yen
-from lib import threshOtsu, points2Img3D
+from skimage.measure import label, regionprops
+from skimage.morphology import closing, square
+from lib import threshOtsu, points2Img3D, belongsToArr, pointsInRadius
 
 class VolumeData():
     """
@@ -195,6 +197,117 @@ class VolumeData():
         print('Znalezione prgi: ', threshList)
         
         return segVolume
+
+    def regionGrowingSegment(self, c=2, sigma0=20):
+        """
+        Wykonuje segmentację algorytmem Region Growing. Za punkt startowy bierze punkt z obszaru zsegmentowantego progowaniem Otsu.
+        Wartość zwracana
+            Posegmentowana macierz 3D: VolumeData
+        """
+        img = self._data3D
+
+        trzyDe = True
+        if len(img.shape) == 2:
+            trzyDe = False
+
+        thresh = threshold_yen(img)
+        bw = img >= thresh
+        label_image = label(bw)
+
+        centroidy = []
+        for region in regionprops(label_image):
+            centroidy.append(region.centroid)
+
+        centroidy = np.array(centroidy)
+        startPoints = np.around(centroidy).astype(int)
+        regions = []
+        for startPoint in startPoints:
+
+            if startPoint is None:
+                if trzyDe:
+                    startPoint = [0,0,0]
+                else:
+                    startPoint = [0,0]
+
+            # Parametr algorytmu
+            c = 2
+
+            region = [startPoint]
+
+            # Transformacje, które pozwolą na multiindeksowanie macierzy/obrazu
+            regionArr = np.array(region)
+            if trzyDe:
+                tempImg = img[regionArr[:,0], regionArr[:,1], regionArr[:,2]]
+            else:
+                tempImg = img[regionArr[:,0], regionArr[:,1]]
+
+            avg = np.mean(tempImg)
+            sigma0 = 20
+            sigmaC = sigma0
+
+            licznikTrafien = 0
+            R = 1
+
+            # Pętla po pikselach/wokselach. Wychodząc z punktu startPoint promieniście rozchodzi się algorytm.
+            while(True):
+                print(f'Promień: {R}')
+                points = pointsInRadius(startPoint, R)
+                if not belongsToArr(points, img):    # Sprawdzenie, czy przynajmniej jeden punkt należy do macierzy
+                    print('Wyjście poza macierz wszystkich punktów.')
+                    break
+
+                licznikTrafien = 0
+                for p in points:
+                    # p - współrzędne punktu 
+                    if belongsToArr(p, img):
+                        print(f'Punkt {p}: {img[p[0], p[1], p[2]]}')
+                        if trzyDe:
+                            tempWynik = abs(img[p[0], p[1], p[2]] - avg)
+                        else:
+                            tempWynik = abs(img[p[0], p[1]] - avg)
+                        print(f'tempWynik: {tempWynik}, sigmaC: {sigmaC}')
+
+                        if tempWynik <= sigmaC:
+                            region.append(p)
+                            print('Dodany')
+                            licznikTrafien += 1
+
+                            # Transformacje, które pozwolą na multiindeksowanie macierzy/obrazu
+                            regionArr = np.array(region)
+                            
+                            if trzyDe:
+                                tempImg = img[regionArr[:,0], regionArr[:,1], regionArr[:,2]]
+                            else:
+                                tempImg = img[regionArr[:,0], regionArr[:,1]]
+        
+                            avg = np.mean(tempImg)
+                            sigmaC = c * np.sqrt(np.var(tempImg))
+
+                            if sigmaC == 0:
+                                sigmaC = sigma0
+                
+                            print('srednia:', avg)
+                            print('sigma:', sigmaC)
+                
+                # Jeśli licznik jest 0 to zakończ pętlę
+                if licznikTrafien == 0 :
+                    print('Brak trafień dla promienia R=', R)
+                    break
+
+                R += 1
+        r = np.array(region)
+        regions.append(r)
+
+        newImg = np.zeros(img.shape, dtype='uint8')
+
+        for r in regions:
+            if trzyDe:
+                newImg[r[:,0], r[:,1], r[:,2]] = 255
+            else:
+                newImg[r[:,0], r[:,1]] = 255
+
+        segImage = VolumeData(newImg)
+        return segImage
 
 
     def segmentDataByThresholds(self, ths):
