2f48dcfe44e21e5139f2f75e00e732a16eb75cb5
Krzysztof Krupiński <kkrupins@mion.elka.pw.edu.pl>
2019-05-18 15:31:36 +0200

    Poprawiona metoda getListOfFiles

--- lib.py
+++ lib.py
@@ -176,19 +176,13 @@ def points2Img3D(rawData):
     return img3D
 
 def getListOfFiles(dirName):
-    # create a list of file and sub directories 
-    # names in the given directory 
-    listOfFile = os.listdir(dirName)
-    allFiles = list()
-    # Iterate over all the entries
-    for entry in listOfFile:
-        # Create full path
-        fullPath = os.path.join(dirName, entry)
-        # If entry is a directory then get the list of files in this directory 
-        if os.path.isdir(fullPath):
-            allFiles = allFiles + getListOfFiles(fullPath)
-        else:
-            allFiles.append(fullPath)
+    """
+    Tworzy listę wszystkich plików w danej lokalizacji
+    """
+    allFiles = []
+    for root, dirs, files in os.walk('/home/krzysztof/Pulpit/Praca_inż'):
+        for name in files:
+            allFiles.append(os.path.join(root, name))
                 
     return allFiles
 
